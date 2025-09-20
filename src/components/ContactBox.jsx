7cfe884cc7d3564d8ff737d693434279d9278508
Dev-Soares <bernardo.soares30@outlook.com>
2025-09-20 11:28:46 -0300

    teste de fontes

--- src/components/ContactBox.jsx
+++ src/components/ContactBox.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const ContactBox = ({contactIcon, contactLink, delay}) => {
   return (
     <div  data-aos="fade-down" data-aos-duration="1500" data-aos-easing="ease-in-out" data-aos-delay={delay}>
-      <a href={contactLink} className='w-auto h-full flex justify-center items-center border-2 border-[var(--color-main)] rounded-2xl py-2  hover:bg-[var(--color-main)] transition-all hover:scale-105 duration-1000 cursor-pointer max-w-55 max-h-32 shadow-md shadow-purple-600' target="_blank">
+      <a href={contactLink} className='w-auto h-full flex justify-center items-center border-2 border-[var(--color-main)] rounded-2xl py-2  hover:bg-[var(--color-main)] transition-all ease-in-out hover:scale-105 duration-1000 cursor-pointer max-w-55 max-h-32 shadow-md shadow-purple-600' target="_blank">
             <img src={ contactIcon } alt="Icone Contato" className={` w-[50%]`}/>
     </a>
     </div>
